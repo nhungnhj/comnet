adc15bf2e43346e7936789f954571160455ae835
nhungnhj <108037879+nhungnhj@users.noreply.github.com>
2022-12-19 02:18:26 +0900

    ghgh

--- file_send_management.py
+++ file_send_management.py
@@ -11,7 +11,7 @@ code101 = "NG 101 No such file"     #ファイルが存在しない
 code102 = "NG 102 Invalid range"    #指定されたファイルの範囲が不適
 code301 = "NG 301 INvalid command"  #コマンドが間違っている
 
-only_server_port = 53922 # chuukei
+only_server_port = 50306 # chuukei
 server_port = 60623 # host contains files
 
 def rep(fserver_name, fname, key, got_data):
