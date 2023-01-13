010c6ecada077174ecd46df6374093358973c0a8
nhungnhj <108037879+nhungnhj@users.noreply.github.com>
2023-01-13 20:27:25 +0900

    2 relay

--- file_send_management.py
+++ file_send_management.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# chuukei
+# chuukei 
 import random
 from socket import *
 import threading
@@ -11,32 +11,33 @@ code101 = "NG 101 No such file"     #ファイルが存在しない
 code102 = "NG 102 Invalid range"    #指定されたファイルの範囲が不適
 code301 = "NG 301 INvalid command"  #コマンドが間違っている
 
-only_server_port = 53924 # chuukei
+only_server_port = 53932 # chuukei
 server_port = 60623 # host contains files
 
 def rep(fserver_name, fname, key, got_data):
-    try: #rep
-        fserver_socket = socket(AF_INET, SOCK_STREAM)
-        fserver_socket.connect((fserver_name, server_port))
-        fil = open(fname, 'w')
-        fil.write(got_data.decode())
-        repkey_out = pbl2.repkey(key, fname)
-        rep = "REP" + " " + fname + " " + repkey_out + "\n"
-        fserver_socket.send(rep.encode())
-        recv_bytearray = bytearray()
-        while True:
-            recv_rep = fserver_socket.recv(1)[0]
-            recv_bytearray.append(recv_rep)
-            if recv_rep == 0x0a:
-                break
-        print('From Server: {0}'.format(recv_bytearray.decode()))
-    except:
-        print("Unexpected Error")
+    #try: #rep
+    fserver_socket = socket(AF_INET, SOCK_STREAM)
+    fserver_socket.connect((fserver_name, server_port))
+    fil = open(fname, 'wb')
+    fil.write(got_data.decode())
+    repkey_out = pbl2.repkey(key, fname)
+    rep = "REP" + " " + fname + " " + repkey_out + "\n"
+    fserver_socket.send(rep.encode())
+    recv_bytearray = bytearray()
+    while True:
+        recv_rep = fserver_socket.recv(1)[0]
+        recv_bytearray.append(recv_rep)
+        if recv_rep == 0x0a:
+            break
+    print('From Server: {0}'.format(recv_bytearray.decode()))
+    #except:
+    #    print("Unexpected Error")
 
 def interact_with_client(client_connect): 
+    partial_data = bytearray()
+    all_data = bytearray()
     while True:
         receive(client_connect) #クライアントからの命令を実行
-    client_connect.close()
 
 def receive(client_connect):
     sentence = client_connect.recv(1024).decode()
@@ -74,13 +75,14 @@ def receive(client_connect):
         recv_data = fserver_socket.recv(int(arr[7])) #指定したバイト数受け取る
         
         stop_time = time.time() #計測終了
+        global partial_data
+        partial_data = recv_data
         fserver_socket.close() #ファイルサーバから切断
         recv_time = stop_time - start_time #受け取るのにかかった時間を計算
         send_relay = str(recv_time) + " " + "sec" + "\n"
         client_connect.send(send_relay.encode()) #時間をクライアントに送信
         print(recv_time)
         print("計測時間送信完了")
-
     elif com == 'DL' and move == 'ALL':  #クライアントからDLを受け取ったら(ALL)
         fserver_name = fserver
         fserver_socket = socket(AF_INET, SOCK_STREAM) #ファイルサーバに接続
@@ -105,13 +107,67 @@ def receive(client_connect):
             got_data += recv_data
             if len(recv_data) <= 0:
                 break
+        global all_data
+        all_data = got_data
         fserver_socket.close()
         #rep(fserver_name, fname, key, got_data)
-        client_connect.send(got_data)
-        print("すべてのファイルを転送完了")
-    else:
-        print(code301)             
+        #client_connect.send(got_data)
+        print("すべてのファイルを受信完了")
+        messege = "すべてのファイルを受信完了\n"
+        client_connect.send(messege.encode())
+    elif com == 'DLrelay' and move == 'PARTIAL':
+        relay_server_name = fserver
+        relay_server_socket = socket(AF_INET, SOCK_STREAM)
+        relay_server_socket.connect((relay_server_name, only_server_port))
 
+        send_get_partial = "GETrelay" + " " + "dummy" + " " + "dummy" + " " + fname + " " + key + " " + move + " " + str(0) + " " + str(9) + "\n"
+        #GETrelay_dummy_dummy_ファイル名_key_partial_0_9\n
+        relay_server_socket.send(send_get_partial.encode())
+        print("GET要求送信")
+        recv_data_partial = bytearray()
+        start_time = time.time()
+        recv_data_partial = relay_server_socket.recv(int(arr[7]))
+        print(recv_data_partial)
+        stop_time = time.time()
+        relay_server_socket.close()
+        recv_time = stop_time - start_time
+        send_relay = str(recv_time) + " " + "sec" + "\n"
+        client_connect.send(send_relay.encode())
+        print(recv_time)
+        print("計測時間送信完了")
+    elif com == 'DLrelay' and move == 'ALL':
+        relay_server_name = fserver
+        relay_server_socket = socket(AF_INET, SOCK_STREAM) #ファイルサーバに接続
+        relay_server_socket.connect((relay_server_name, only_server_port))
+
+        send_get_all = "GETrelay" + " " + "dummy" + " " + "dummy" + " " + fname + " " + key + " " + move + "\n"
+        #GETrelay_dummy_dummy_ファイル名_key_ALL\n
+        relay_server_socket.send(send_get_all.encode())
+        print("GET要求送信")
+
+        recv_data = bytearray()
+        got_data = bytearray()
+        while True:
+            recv_data = relay_server_socket.recv(1024)
+            got_data += recv_data
+            if len(recv_data) <= 0:
+                break
+        print("全てのデータを受信2")
+        relay_server_socket.close()
+        client_connect.send(got_data)
+        print("クライアントにファイルを送信")
+    else:
+        print(code301)
+        
+    if com == 'GETrelay' and move == 'PARTIAL':
+        print("一部データを送信")
+        client_connect.send(partial_data)
+    elif com == 'GETrelay' and move == 'ALL':
+        print("全てのデータを送信")
+        client_connect.send(all_data)
+
+    client_connect.close()
+    
 if __name__ == '__main__':
     server_socket = socket(AF_INET, SOCK_STREAM)  # TCPを使う待ち受け用のソケットを作る
     server_socket.bind(('', only_server_port))  # ポート番号をソケットに対応づける
